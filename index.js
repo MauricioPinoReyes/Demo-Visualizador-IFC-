b4b619ac8405cf66c8e8025ac8b5175a04e1d2a4
Mauricio Pino <mauricio.pino.log@gmail.com>
2023-07-12 22:43:17 -0400

    se agrega visibilidad y cambio de estilos css

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 import { Color } from 'three';
 import { IfcViewerAPI } from 'web-ifc-viewer';
-import { IFCWALLSTANDARDCASE, IFCSLAB } from 'web-ifc';
+import {
+	IFCWALLSTANDARDCASE,
+	IFCSLAB,
+	IFCDOOR,
+	IFCWINDOW,
+	IFCFURNISHINGELEMENT,
+	IFCMEMBER,
+	IFCPLATE,
+} from 'web-ifc';
+
 
 const container = document.getElementById('viewer-container');
-const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xffffff) });
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0.2, 0.2, 0.2) });
 viewer.grid.setGrid();
 viewer.axes.setAxes();
 
 async function loadIfc(url) {
     await viewer.IFC.setWasmPath("wasm/");
     const model = await viewer.IFC.loadIfcUrl(url);
+
+    model.removeFromParent();
+
     await viewer.shadowDropper.renderShadow(model.modelID);
     viewer.context.renderer.postProduction.active = true;
+
+   
 
     const ifcProject = await viewer.IFC.getSpatialStructure(model.modelID);
     createTreeMenu(ifcProject);
@@ -19,6 +33,10 @@ async function loadIfc(url) {
     const walls = await viewer.IFC.getAllItemsOfType(model.modelID, IFCWALLSTANDARDCASE, true);
 
     const slabs = await viewer.IFC.getAllItemsOfType(model.modelID, IFCSLAB, true);
+
+    /*visibility*/
+    await setupAllCategories();
+    
 
     const wallsTable = document.getElementById('walls-table');
     const tBodyWalls = wallsTable.querySelector('tbody');
@@ -57,6 +75,7 @@ async function loadIfc(url) {
 }
 
 loadIfc('./06.ifc');
+
 
 // Tree view
 
@@ -203,7 +222,7 @@ function removeAllChildrenProperties(element) {
 const accordion = document.getElementsByClassName('contentBX');
 
 for (i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', function () {
+    accordion[i].addEventListener('dblclick', function () {
         this.classList.toggle('active-2')
     })
 }
@@ -252,3 +271,72 @@ function decodeIFCString(ifcString) {
     return resultString;
 }
 /*END EXPORT TO EXCEL*/
+
+/*VISIBILITY*/
+
+const scene = viewer.context.getScene();
+
+// List of categories names
+const categories = {
+	IFCWALLSTANDARDCASE,
+	IFCSLAB,
+	IFCFURNISHINGELEMENT,
+	IFCDOOR,
+	IFCWINDOW,
+	IFCPLATE,
+	IFCMEMBER,
+};
+
+// Gets the name of a category
+function getName(category) {
+	const names = Object.keys(categories);
+	return names.find(name => categories[name] === category);
+}
+
+// Gets all the items of a category
+async function getAll(category) {
+	return viewer.IFC.loader.ifcManager.getAllItemsOfType(0, category, false);
+}
+
+// Creates a new subset containing all elements of a category
+async function newSubsetOfType(category) {
+	const ids = await getAll(category);
+	return viewer.IFC.loader.ifcManager.createSubset({
+		modelID: 0,
+		scene,
+		ids,
+		removePrevious: true,
+		customID: category.toString(),
+	});
+}
+
+// Stores the created subsets
+const subsets = {};
+
+async function setupAllCategories() {
+	const allCategories = Object.values(categories);
+	for (let i = 0; i < allCategories.length; i++) {
+		const category = allCategories[i];
+		await setupCategory(category);
+	}
+}
+
+// Creates a new subset and configures the checkbox
+async function setupCategory(category) {
+	subsets[category] = await newSubsetOfType(category);
+	setupCheckBox(category);
+}
+
+// Sets up the checkbox event to hide / show elements
+function setupCheckBox(category) {
+	const name = getName(category);
+	const checkBox = document.getElementById(name);
+	checkBox.addEventListener('change', (event) => {
+		const checked = event.target.checked;
+		const subset = subsets[category];
+		if (checked) scene.add(subset);
+		else subset.removeFromParent();
+	});
+}
+
+/*END VISIBILITY*/
